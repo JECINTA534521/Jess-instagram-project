bb37b37e8b2634ad903952bea0507ed75e58b8a6
jecinta <jecintawanjirug@gmail.com>
2019-11-19 08:58:54 +0300

    this adds a url to edit the profile

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,7 @@ from .views import FollowUser, ViewFollowers
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-   # path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('edit_profile/', views.edit_profile, name='edit_profile'),
     path('password_reset/',
