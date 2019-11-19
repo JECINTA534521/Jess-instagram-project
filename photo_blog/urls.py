39b66c117adb248a38a41a401ccabb1ff146b0dc
jecinta <jecintawanjirug@gmail.com>
2019-11-19 16:32:22 +0300

    this changes the path to url

--- photo_blog/urls.py
+++ photo_blog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.conf.urls import url
 from . import views
 from .views import (
 Home, ViewPost, CreatePost,
@@ -9,16 +9,16 @@ LikePostAPI, ViewLikes, ViewNotifications
 
 
 urlpatterns = [
-    path('', Home.as_view(), name='photo_blog-home'),
-    path('search/', views.search, name='search'),
-    path('post/<int:pk>/', ViewPost.as_view(), name='photo_blog-detail'),
-    path('post/new/', CreatePost.as_view(), name='photo_blog-create'),
-    path('post/<int:pk>/update', UpdatePost.as_view(), name='photo_blog-update'),
-    path('post/<int:pk>/delete/', DeletePost.as_view(), name='photo_blog-delete'),
-    path('user/<str:username>/', ViewProfile.as_view(), name='photo_blog-profile'),
-    path('post/<int:pk>/comment/', CreateComment.as_view(), name='photo_blog-comment'),
-    path('comment/<int:pk>/delete/', DeleteComment.as_view(), name='photo_blog-delete_comment'),
-    path('post/<int:pk>/like_api/', LikePostAPI.as_view(), name='photo_blog-post_like_api'),
-    path('post/<int:pk>/likes/', ViewLikes.as_view(), name='photo_blog-post_likes'),
-    path('notifications/', ViewNotifications.as_view(), name='photo_blog-notifications'),
+    url('', Home.as_view(), name='photo_blog-home'),
+    url('search/', views.search, name='search'),
+    url('post/<int:pk>/', ViewPost.as_view(), name='photo_blog-detail'),
+    url('post/new/', CreatePost.as_view(), name='photo_blog-create'),
+    url('post/<int:pk>/update', UpdatePost.as_view(), name='photo_blog-update'),
+    url('post/<int:pk>/delete/', DeletePost.as_view(), name='photo_blog-delete'),
+    url('user/<str:username>/', ViewProfile.as_view(), name='photo_blog-profile'),
+    url('post/<int:pk>/comment/', CreateComment.as_view(), name='photo_blog-comment'),
+    url('comment/<int:pk>/delete/', DeleteComment.as_view(), name='photo_blog-delete_comment'),
+    url('post/<int:pk>/like_api/', LikePostAPI.as_view(), name='photo_blog-post_like_api'),
+    url('post/<int:pk>/likes/', ViewLikes.as_view(), name='photo_blog-post_likes'),
+    url('notifications/', ViewNotifications.as_view(), name='photo_blog-notifications'),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,4 @@
-from django.urls import path
-
+from django.conf.urls import url
 from . import views
 from django.contrib.auth import views as auth_views
 from .views import FollowUser, ViewFollowers
@@ -7,24 +6,24 @@ from .views import FollowUser, ViewFollowers
 
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('edit_profile/', views.edit_profile, name='edit_profile'),
-    path('password_reset/',
+    url('register/', views.register, name='register'),
+    url('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    url('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    url('edit_profile/', views.edit_profile, name='edit_profile'),
+    url('password_reset/',
           auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),
           name='password_reset'),
-    path('password_reset/done/',
+    url('password_reset/done/',
           auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
           name='password_reset_done'),
-    path('password_reset_confirm/<uidb64>/<token>/',
+    url('password_reset_confirm/<uidb64>/<token>/',
           auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
           name='password_reset_confirm'),
-    path('password_reset_complete/',
+    url('password_reset_complete/',
           auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
           name='password_reset_complete'),
-    path('user/<str:username>/follow/', FollowUser.as_view(), name='user_follow'),
-    path('user/<str:username>/followers/', ViewFollowers.as_view(), name='user_followers'),
+    url('user/<str:username>/follow/', FollowUser.as_view(), name='user_follow'),
+    url('user/<str:username>/followers/', ViewFollowers.as_view(), name='user_followers'),
 
 
 ]
